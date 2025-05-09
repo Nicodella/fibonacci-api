667ec53447b82302192c34d045828db1dbc311eb
Rodrigo <r.nicodella12@gmail.com>
2025-05-09 00:08:31 -0300

    Se corrige mensaje de los endpoint

--- src/main/java/com/fibonacci/fibonacci_api/controller/FibonacciController.java
+++ src/main/java/com/fibonacci/fibonacci_api/controller/FibonacciController.java
@@ -30,14 +30,14 @@ public class FibonacciController {
     }
     
     @GetMapping("/{n}")
-    @Operation(summary = "Get Fibonacci(n)", description = "Calcula Fibonacci de un numero n. Para el caso 0 que no existe, se devuelve -1")
+    @Operation(summary = "Get Fibonacci(n)", description = "Calcula Fibonacci de un numero n.")
     public ResponseEntity<String> getFibonacci(@PathVariable int n) {
         if (n < 0) return ResponseEntity.badRequest().body("Fibonacci(n negativo) no existe");
         return ResponseEntity.ok(service.getFibonacci(n));
     }
 
     @GetMapping("/{n}/cantaccesos")
-    @Operation(summary = "Get cantidad de accesos de n", description = "Devuelve la cantidad de accesos de n. Para el caso 0 que no existe, se devuelve -1 y si n no esta en base devuelve -2")
+    @Operation(summary = "Get cantidad de accesos de n", description = "Devuelve la cantidad de accesos de n.")
     public ResponseEntity<String> getAccessCount(@PathVariable int n) {
         if (n < 0) return ResponseEntity.badRequest().body("Fibonacci(n negativo) no existe");
         String cant = service.getCantAccesos(n);
